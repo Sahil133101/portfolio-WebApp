939933cfc40cc79939d65c0848baf35ec09d8728
Sahil Sharma <103818219+Sahil133101@users.noreply.github.com>
2024-02-18 17:58:28 +0530

    commit

--- src/Routes/Project.js
+++ src/Routes/Project.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import NavBar from "../Component/NavBar";
-import './Project.css';
+// import './Project.css';
 import Footer from "../Component/Footer";
 
 const Project = () => {
   return (
-    <div>
-      <div className='navbar'>
+    
+        <>
         <NavBar/>
-      </div>
-      <div className='project'>
+        <br></br>
+       
         <Footer/>
-      </div>
-    </div>
+        </>
+    
   );
 }
 
