a6c2c994a12a680f5bf1f1db2d3c86df939c4433
Sahil Sharma <103818219+Sahil133101@users.noreply.github.com>
2024-02-18 16:05:55 +0530

    Practice

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,6 +1,6 @@
 import "./Footer.css";
 import React from 'react';
-import { FaHome, FaPhone } from "react-icons/fa";
+import { FaHome, FaPhone, FaMailBulk, FaFacebook, FaTwitter, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -8,24 +8,38 @@ const Footer = () => {
       <div className="footer-container">
         <div className="left">
           <div className="location">
-            <FaHome size={20} style={{ color: "white", marginRight: "1rem" }} />
+            <FaHome size={20} style={{ color: "white", marginRight: "10px" }} />
             <div>
-              <p>39KA, Near Post Office</p>
-              <p>Moh Mahadev Nakur</p>
-              <p>Distt. Saharanpur</p>
-            </div>
-            </div>
-            <div className="phone">
-            <FaPhone size={20} style={{ color: "white", marginRight: "1rem" }} />
-            <div>
-              <p>6396632572</p>
+              <p style={{ padding: "5px 0", marginBottom: 0 }}>Near Pioneer School</p>
+              <p style={{ padding: "5px 0", marginBottom: 0 }}> Singhpura</p>
+              <p style={{ padding: "5px 0", marginBottom: 0 }}>Distt. Jind</p>
             </div>
           </div>
-          
+          <div className="phone">
+            <FaPhone size={20} style={{ color: "white", marginRight: "10px" }} />
+            <p style={{ padding: "5px 0", marginBottom: 0 }}>+91-73u29u929</p>
+          </div>
+          <div className="gmail">
+            <FaMailBulk size={20} style={{ color: "white", marginRight: "10px" }} />
+            <p style={{ padding: "5px 0", marginBottom: 0 }}>brar@gmail.com</p>
+          </div>
         </div>
 
         <div className="right">
-          
+          <p> <h3> About the company</h3></p>
+          <p> This is me Manpreet Brar.  CEO & founder of Brar Kotha.  I enjoy with every Customer</p>
+          <div className="Social">
+          <a href="https://www.facebook.com/your-profile-link">
+              <FaFacebook size={30} style={{ color: "white", marginRight: "1rem" }} />
+            </a>
+            <a href="https://www.twitter.com/your-profile-link">
+              <FaTwitter size={30} style={{ color: "white", marginRight: "1rem" }} />
+            </a>
+            <a href="https://www.linkedin.com/your-profile-link">
+              <FaLinkedin size={30} style={{ color: "white", marginRight: "1rem" }} />
+            </a>
+               </div>
+               
         </div>
       </div>
     </div>
