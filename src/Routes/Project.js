6d61152d2a121d7a40eac87fd9107610f363e52f
Sahil Sharma <103818219+Sahil133101@users.noreply.github.com>
2024-03-19 21:25:13 +0530

    practice

--- src/Routes/Project.js
+++ src/Routes/Project.js
@@ -2,28 +2,26 @@ import React from 'react';
 import NavBar from "../Component/NavBar";
 import './Project.css';
 import Footer from "../Component/Footer";
-import Introimg from "../assests/wallpaper.jpg"
+import Introimg from "../assests/wallpaper.jpg";
 import PricingCard from '../Component/PricingCard';
-
 import Hero2Image from '../Component/Hero2Image';
+import WorkCard from '../Component/WorkCard';
+
 
 const Project = () => {
   return (
-    
-        <>
-        <div className='project'>
-        <NavBar/>
-        
-{/*         
-        <img className=" intro-img" 
-        src={Introimg} alt="IntroImg"/> */}
-        <Hero2Image heading="PROJECTS." text= "Some of my most recent works"/>
-        <PricingCard></PricingCard>
-        <Footer/>
-        </div>
+    <>
+      <div className='project'>
+        <NavBar />
+        {/* Commented out the intro image as it's not present in the code */}
+        {/* <img className="intro-img" src={Introimg} alt="IntroImg" /> */}
+        <Hero2Image heading="PROJECTS." text="Some of my most recent works" />
+        <PricingCard /> {/* Corrected the component name */}
+        <WorkCard></WorkCard>
        
-        </>
-    
+        <Footer />
+      </div>
+    </>
   );
 }
 
