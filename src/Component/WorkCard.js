0148ef39f3f397b8d0329608224787802867cf2d
Sahil Sharma <103818219+Sahil133101@users.noreply.github.com>
2024-03-20 22:14:03 +0530

    Practice

--- src/Component/WorkCard.js
+++ src/Component/WorkCard.js
@@ -11,40 +11,48 @@ const workcard = () => {
     <div className="project-container">
       <div className="Project-Card">
         <img src={pro1} alt= "image "/>
-        <h2 className="Project Title">Project Title </h2>
+        
+         <div className="Heading2">  <h2 className="Project Title">Project Title </h2>
+        </div>
         
         <div className="ProDetails"><p>This is a Bluetooth control robot using Arduino</p></div>
+        <div className="buttons">
         <div className="pro-btns">
           <NavLink to="https://github.com/Sahil133101" className="btn">View</NavLink> </div>
           <div className="pro-btns">
           <NavLink to="https://github.com/Sahil133101" className="btn">Source</NavLink> </div>
-          
+          </div>
           </div>
         <div className="Project-Card">
         <img src={pro1} alt= "image "/>
-        <h2 className="Project Title">Project Title </h2>
         
-        <div className="ProDetails"><p>This is a Bluetooth control robot using Arduino</p></div>
-        <div className="pro-btn">
-          <NavLink to="https://github.com/Sahil133101" className="btn">View</NavLink> </div>
-          <div className="pro-btn">
-          <NavLink to="https://github.com/Sahil133101" className="btn">Source</NavLink> </div>
-          
+        <div className="Heading2">
+        <h2 className="Project Title">Project Title </h2>
         </div>
+        <div className="ProDetails"><p>This is a Bluetooth control robot using Arduino</p></div>
+        <div className="buttons"> 
+        <div className="pro-btns">
+          <NavLink to="https://github.com/Sahil133101" className="btn">View</NavLink> </div>
+          <div className="pro-btns">
+          <NavLink to="https://github.com/Sahil133101" className="btn">Source</NavLink> </div>
+          </div>
         </div>
         <div className="Project-Card">
         <img src={pro1} alt= "image "/>
-        <h2 className="Project Title">Project Title </h2>
+        <h2 className="Project-Title">Project Title </h2>
         
         <div className="ProDetails"><p>This is a Bluetooth control robot using Arduino</p></div>
-        <div className="pro-btn">
+        <div className="buttons">
+        <div className="pro-btns">
           <NavLink to="https://github.com/Sahil133101" className="btn">View</NavLink> </div>
-          <div className="pro-btn">
+          <div className="pro-btns">
           <NavLink to="https://github.com/Sahil133101" className="btn">Source</NavLink> </div>
-          
+          </div>
         </div>
         </div>
+        
         </div>
+        
   )
 }
 
